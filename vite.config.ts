b9554dbc3813ea2c0f9e168254bea4f045d59beb
Melquiades Bustamante <bonfil99melkia@gmail.com>
2023-02-12 13:40:20 -0600

    ci: semantic release workflow

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
+import { resolve } from 'path'
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: "melkiads",
+      fileName: "main",
+      formats: ["es"]
+    },
+    rollupOptions: {
+      external: ["react", "react-dom", "react-jss"],
+      
+    },
+  },
   plugins: [react()],
 });
